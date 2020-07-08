d49135a9ca6cdfc74ff08d6e7ebed21d613f1ef5
Michael Haber <michaeladam.haber@gmail.com>
2020-07-08 19:30:16 -0400

    Experimented with retrieving data through local storage

--- highscores.js
+++ highscores.js
@@ -3,11 +3,13 @@ var clearScores = document.getElementById("clear-highscores");
 var scoresList = document.getElementById("list");
 var input = document.getElementById("user-input");
 
+// JSON.parse(localStorage.getItem("Initials & Score"));
+
 submitScore.addEventListener("click", function () {
   console.log(input.value);
   var newScore = document.createElement("li");
   var userInput = document.createTextNode(input.value);
   newScore.appendChild(userInput);
   document.getElementById("list").appendChild(newScore);
-  localStorage.setItem("Initials & Score", input.value);
+  //   localStorage.setItem("Initials & Score", JSON.stringify(input.value));
 });
